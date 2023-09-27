5af38c3ff8fcba35a7b1dcbbc788562a8bc6644a
vaneval <vaneval.dese@gmail.com>
2023-09-26 20:52:03 -0500

    Contador de palabras

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// Captura de los elementos del DOM
+const textArea = document.querySelector('textarea');
+const resetBtn = document.getElementById('reset-button');
+const wordCountElement = document.querySelector('li.metric:nth-child(1)');
+const charCountElement = document.querySelector('li.metric:nth-child(2)');
+const charCountNoSpacesElement = document.querySelector('li.metric:nth-child(3)');
+const numbersCountElement = document.querySelector('li.metric:nth-child(4)');
+const numberSumElement = document.querySelector('li.metric:nth-child(5)');
+const averageWordLengthElement = document.querySelector('li.metric:nth-child(6)');
+
+
+// Función para actualizar las métricas
+function updateMetrics() {
+    const text = textArea.value;
+
+  //Invocación métodos del objeto Analyzer
+
+   //Ejemplo: Input: "Hola, devs 0 12 de Laboratoria!" -> Palabras: 6  
+    const words = analyzer.getWordCount(text);
+    console.log(words)
+    //wordCountElement.textContent = analyzer.getWordCount(text);
+    wordCountElement.textContent = `Palabras: ${words}`;
+
+   
+
+
+
+}
+
+//Evento input en el textArea
+textArea.addEventListener('input', updateMetrics);
+
+//TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
+
+  
